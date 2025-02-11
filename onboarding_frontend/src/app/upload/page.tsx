616a0d17191f8644c2306711c2a9356d8b4f775c
Anders <anders.vold.hoseth@gmail.com>
2025-02-11 10:10:55 +0100

    replaced console app with simple asp.net backend, /upload now talks to backend

---
+++ onboarding_frontend/src/app/upload/page.tsx
@@ -0,0 +1,32 @@
+// pages/upload.tsx
+import React from 'react';
+import FileUploader from '../components/FileUploader';
+
+export default function UploadPage() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
+        <h1 className="text-3xl sm:text-4xl font-bold text-center">
+          Upload your files
+        </h1>
+    
+        <p className="text-center">
+          Get started by uploading your files to our server.
+        </p>
+    
+        {/* Render the FileUploader component here */}
+        <div className="mx-auto">
+          <FileUploader />
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/"
+        >
+          Home
+        </a>
+      </footer>
+    </div>
+  );
+}
