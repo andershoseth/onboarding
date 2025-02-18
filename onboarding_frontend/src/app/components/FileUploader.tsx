cedd92e47ffa146f8983d8a445d1d74acb4a7248
salut7hello <hch028@post.uit.no>
2025-02-18 13:43:07 +0100

    backup

--- onboarding_frontend/src/app/components/FileUploader.tsx
+++ onboarding_frontend/src/app/components/FileUploader.tsx
@@ -1,18 +1,16 @@
 'use client'
-import { useState } from "react";
+// components/FileUploader.tsx
+import { ChangeEvent, useState } from "react";
 
 export default function FileUploader() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-<<<<<<< HEAD
-  const [uploadResult, setUploadResult] = useState<unknown>(null);
-=======
   const [uploadResponse, setUploadResponse] = useState<{ message: string; fileName: string } | null>(null);
->>>>>>> origin/main
 
-  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if (!e.target.files) return;
-    setSelectedFile(e.target.files[0]);
-  }
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
 
   const uploadFile = async () => {
     if (!selectedFile) return;
@@ -26,19 +24,14 @@ export default function FileUploader() {
         body: formData,
       });
 
-      if (!res.ok) {
-        throw new Error(`Error! status: ${res.status}`);
+      interface UploadResponse {
+        message: string;
+        fileName: string;
       }
 
-<<<<<<< HEAD
-      const data = await res.json();
-      setUploadResult(data); // ⬅ Lagrer responsen i state
-
-=======
       const data: UploadResponse = await res.json();
       setUploadResponse(data);
       console.log(data);
->>>>>>> origin/main
     } catch (error) {
       console.error("Error uploading file:", error);
     }
@@ -47,22 +40,8 @@ export default function FileUploader() {
   return (
     <>
     <div>
-      <h1>File Uploader</h1>
-
       <input type="file" onChange={handleFileChange} />
-<<<<<<< HEAD
-      <button onClick={uploadFile}>Upload</button>
-
-      {/* Viser JSON på nettsiden i en PRE-blokk */}
-      {uploadResult && (
-        <div style={{ marginTop: "1rem" }}>
-          <h2>Upload Result:</h2>
-          <pre>{JSON.stringify(uploadResult, null, 2)}</pre>
-        </div>
-      )}
-=======
       <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-800" onClick={uploadFile}>Upload</button>
->>>>>>> origin/main
     </div>
     <div>
       {uploadResponse && <div>{uploadResponse.message}<br/>{uploadResponse.fileName}</div>}
--- onboarding_frontend/src/app/upload/page.tsx
+++ onboarding_frontend/src/app/upload/page.tsx
@@ -22,13 +22,8 @@ export default function UploadPage() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-<<<<<<< HEAD
           href="/home"
           >
-=======
-          href="/home"
-        >
->>>>>>> origin/main
           Home
         </a>
       </footer>
