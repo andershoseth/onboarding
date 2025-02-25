eaa250ee2a36f40f51d886d403688a02e90e8117
LC5466 <linnc1065@outlook.com>
2025-02-25 10:18:38 +0100

    added comments

--- onboarding_frontend/src/app/importvelger/page.tsx
+++ onboarding_frontend/src/app/importvelger/page.tsx
@@ -5,14 +5,14 @@ import MenuContainer, { useBoxState } from "../components/menuContainer";
 import CheckBox from "../components/checkBox";
 
 const ImportVelger: React.FC = () => {
-    const { selected, handleBoxChange } = useBoxState();
-    const isDisabled = !Object.values(selected).some(Boolean)
+    const { selected, handleBoxChange } = useBoxState(); /* for the checkboxes */
+    const isDisabled = !Object.values(selected).some(Boolean) /* for the next button */
     return (
         <>
             <MenuContainer>
                 <div className="p-6 bg-gray-100 rounded-lg shadow-sm w-96">
                     <h2 className="text-xl font-semibold text-[#E17044]"> Hva ønsker du å importere? </h2>
-                    <p className="text-gray-600 text-sm mb-4"> Huk av hva du vil importere fra det gamle regnskapssystemet </p>
+                    <p className="text-gray-600 text-sm mb-4"> Huk av hva du vil importere fra filene dine </p>
 
                     <div className="space-y-3">
                         <CheckBox
@@ -33,11 +33,12 @@ const ImportVelger: React.FC = () => {
                     </div>
 
                     <div className="mt-6 flex justify-end">
+                        {/* next button to the upload page */}
                         <Link
                             href={isDisabled ? "#" : "/upload"}
                             className={`px-4 py-2 rounded-md shadow-md transition ${isDisabled
-                                    ? "bg-gray-400 text-gray-200 cursor-not-allowed"
-                                    : "bg-[#E17044] text-white hover:bg-[c85b34]"
+                                ? "bg-gray-400 text-gray-200 cursor-not-allowed" /* no button is checked */
+                                : "bg-[#E17044] text-white hover:bg-[c85b34]" /* button is checked */
                                 }`}
                             aria-disabled={isDisabled}
                         >
