0d8cba16066f3180efe83c1275ed7ca4addd8012
LC5466 <linnc1065@outlook.com>
2025-03-03 09:11:41 +0100

    backup, skal lage animasjon til progressbar

--- onboarding_frontend/src/app/components/ProgressBar.tsx
+++ onboarding_frontend/src/app/components/ProgressBar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useUploadContext } from './UploadContext';
 
 interface ProgressBarProps {
     progress: number;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
+    const { uploadProgress } = useUploadContext();
     return (
         <div className="w-full bg-gray-200 rounded-full h-4">
-            <div className="bg-green-500 h-4 rounded-full" style={{ width: `${progress}` }}>
+            <div className="bg-green-500 h-4 rounded-full" style={{ width: `${progress}%` }}>
             </div>
         </div>
     );
