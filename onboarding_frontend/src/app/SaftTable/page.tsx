b128d28abdd7b67bb0ae588d221763f088e6c728
salut7hello <hch028@post.uit.no>
2025-03-17 13:55:57 +0100

    saft table

--- onboarding_frontend/src/app/SaftTable/page.tsx
+++ onboarding_frontend/src/app/SaftTable/page.tsx
@@ -1,43 +1,31 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useUploadContext } from "../components/UploadContext";
+
 import SaftData ,{ GroupedSaftEntries } from "../components/SaftData";
 
 export default function ResultPage() {
   const [saftData, setSaftData] = useState<GroupedSaftEntries[]>([]);
   const [loading, setLoading] = useState(true);
-  const { selectedFile } = useUploadContext()
+ 
   useEffect(() => {
-    // Hvis ingen fil er valgt, gjør ingenting
-    if (!selectedFile) {
-      setLoading(false);
-      return;
-    }
     const formData = new FormData();
-    formData.append("file", selectedFile);
+    
 
-    fetch("http://localhost:5116/api/upload", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`Feil: ${res.status} ${res.statusText}`);
-        }
-        return res.json();
-      })
+  fetch("http://localhost:5116/api/upload", {
+    method: "POST",
+    body: formData,
+  })
+      .then((res) => res.json())
       .then((fetchedData: GroupedSaftEntries[]) => {
-        console.log("Hentet SafT-data:", fetchedData);
+        console.log("Hentet saf-t fil data:", fetchedData);
         setSaftData(fetchedData);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Feil under henting:", error);
-      })
-      .finally(() => {
         setLoading(false);
       });
-  }, [selectedFile]); // useEffect kjører når selectedFile endres
-
+  }, []);
 
   if (loading) {
     return <div className="p-4">Laster...</div>;
