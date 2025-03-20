106c2d1c553fbc2c6ef744c30da148090486e10e
LC5466 <linnc1065@outlook.com>
2025-03-20 12:15:59 +0100

    la til nye elementer i systemCoverage.tsx

--- onboarding_frontend/src/app/export/systemCoverage.tsx
+++ onboarding_frontend/src/app/export/systemCoverage.tsx
@@ -1,11 +1,11 @@
 export const systemCoverage: { [key: string]: { [key: string]: string[] } } = {
   "Visma": {
-
-    safTSubjects: ["hovedboktransaksjoner", "avdeling"],
-    csvSubjects: ["hovedboktransaksjoner", "kontakter", "avdeling", "tilbudimport"],
+    safTSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter", "prosjekter", "prosjektaktiviteter", "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
+    csvSubjects: ["anleggsmidler", "bestillinger", "faktura CID", "lønnsgrunnlag", "lønnsjustering", "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
   },
+
   "Tripletex": {
-    safTSubjects: ["hovedboktransaksjoner", "kontakter"],
-    csvSubjects: ["hovedboktransaksjoner", "kontakter", "avdeling", "tilbudimport"],
+    safTSubjects: ["anleggsmidler", "bestillinger", "faktura CID", "lønnsgrunnlag", "lønnsjustering", "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
+    csvSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter", "prosjekter", "prosjektaktiviteter", "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
   }
-};
+}; //elementene i listene er tatt fra standardimport-mappa på backenden
