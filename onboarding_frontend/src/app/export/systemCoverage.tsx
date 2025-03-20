bbe0064182569f2ebbae0b3bcd2e3c0f376ba85d
LC5466 <linnc1065@outlook.com>
2025-03-20 12:29:38 +0100

    la til nye elementer i systemCoverage.tsx

--- onboarding_frontend/src/app/export/systemCoverage.tsx
+++ onboarding_frontend/src/app/export/systemCoverage.tsx
@@ -1,11 +1,18 @@
 export const systemCoverage: { [key: string]: { [key: string]: string[] } } = {
   "Visma": {
-    safTSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter", "prosjekter", "prosjektaktiviteter", "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
-    csvSubjects: ["anleggsmidler", "bestillinger", "faktura CID", "lønnsgrunnlag", "lønnsjustering", "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
+    safTSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter",
+      "lønnsgrunnlag", "lønnsjustering", "prosjekter", "prosjektaktiviteter",
+      "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
+
+    csvSubjects: ["anleggsmidler", "bestillinger", "faktura CID", "lønnsgrunnlag", "lønnsjustering",
+      "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
   },
 
   "Tripletex": {
-    safTSubjects: ["anleggsmidler", "bestillinger", "faktura CID", "lønnsgrunnlag", "lønnsjustering", "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
-    csvSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter", "prosjekter", "prosjektaktiviteter", "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
+    safTSubjects: ["anleggsmidler", "bestillinger", "bilag", "faktura CID", "kontakter", "kontoplan", "lønnsgrunnlag",
+      "lønnsjustering", "lønnssaldo hittil i år", "produkter", "timeprisspesifikasjon", "tilbud"],
+
+    csvSubjects: ["avdelinger", "bilag", "bilagslinjer", "kontoplan", "kontakter", "prosjekter",
+      "prosjektaktiviteter", "prosjektfaktureringsmetode", "prosjektmedlemmer", "prosjektstatus"],
   }
 }; //elementene i listene er tatt fra standardimport-mappa på backenden
