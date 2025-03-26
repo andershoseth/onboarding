cad7682fae1ed3169af0b496744dff6a5c465229
LC5466 <linnc1065@outlook.com>
2025-03-26 11:00:31 +0100

    backup

--- onboarding_frontend/src/app/components/StepperBar.tsx
+++ onboarding_frontend/src/app/components/StepperBar.tsx
@@ -1,22 +1,20 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ImportContext from "../components/ImportContext";
 import { useUploadContext } from "@/app/components/UploadContext";
 import { Stepper } from "primereact/stepper";
 import { StepperPanel } from "primereact/stepperpanel";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const Sidebar: React.FC = () => {
-  const router = useRouter();
+  const pathname = usePathname(); //må ha path for å gå til neste steg
   const { selectedSystem, fileName, selectedColumns, selectedFileType } = useContext(ImportContext);
   const { uploadedFiles } = useUploadContext();
 
-  // Might change this, since the circle is not "completed" until an action is taken, NOT if anything is saved
   const isColumnsSelected = Object.values(selectedColumns).some(Boolean);
-
-  // Just adding a variable to check if an uploaded file exists for the condition
   const isExportUploaded = Object.keys(uploadedFiles).length > 0;
 
+  //liste over sider (vurder å fjern completed senere hvis mulig)
   const steps = [
     { label: "Systemvalg", url: "/systemvalg", completed: selectedSystem !== null },
     { label: "Filtype", url: "/filtype", completed: selectedFileType !== null },
@@ -25,14 +23,21 @@ const Sidebar: React.FC = () => {
     { label: "Last opp", url: "/upload", completed: fileName !== null },
   ];
 
-  const activeindex = steps.findIndex((step) => !step.completed);
-  const currentStep = activeindex === -1 ? steps.length - 1 : activeindex;
+  const activeStepIndex = steps.findIndex((step) => step.url === pathname);
+
+  //holder følge på hvilket trinn
+  const [currentStep, setCurrentStep] = useState(activeStepIndex !== -1 ? activeStepIndex : 0);
+  useEffect(() => {
+    setCurrentStep(activeStepIndex !== -1 ? activeStepIndex : 0);
+  }, [pathname]);
 
   return (
     <div className="fixed top-0 left-0 w-64 h-full bg-gray-800 text-white pt-10 flex flex-col items-start pl-6">
       <Stepper activeStep={currentStep} orientation="vertical">
         {steps.map((step, index) => (
-          <StepperPanel key={index} header={step.label}>
+          <StepperPanel
+            key={index}
+            header={step.label}>
           </StepperPanel>
         ))}
       </Stepper>
