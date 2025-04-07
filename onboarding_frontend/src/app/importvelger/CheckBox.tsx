c845523b76c92c57156241cdf16783d807dee5a2
LC5466 <linnc1065@outlook.com>
2025-04-07 13:31:11 +0200

    endret på én fil

--- onboarding_frontend/src/app/importvelger/CheckBox.tsx
+++ onboarding_frontend/src/app/importvelger/CheckBox.tsx
@@ -10,10 +10,10 @@ const CheckBox: React.FC<CheckBoxProps> = ({ label, checked, onChange }) => {
     return (
         <label className="flex items-center space-x-2 cursor-pointer">
             <input type="checkbox" checked={checked} onChange={onChange} className="hidden" />
-            <div className={`w-5 h-5 flex items-center justify-center border-2 rounded-md transition-all ${checked ? "border-[#E17044]" : "border-gray-400"}`}>
+            <div className={`w-5 h-5 flex items-center justify-center border-2 rounded-md transition-all ${checked ? "border-[#E17044]" : "border-black"}`}>
                 {checked && <span className="text-black text-lg">✔</span>}
             </div>
-            <span className="text-gray-700"> {label} </span>
+            <span className="text-black"> {label} </span>
         </label>
     );
 };
